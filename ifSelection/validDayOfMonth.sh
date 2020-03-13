a9991a61b7a02a033003390011e8c32616b58664
USER <prathameshsalap@gmail.com>
2020-03-13 10:02:18 +0530

    ADDED: Displayed True If Date Is Valid Otherwise False

---
+++ ifSelection/validDayOfMonth.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+args=("$@")
+month=${args[0]}
+day=${args[1]}
+
+if [[ $month -ge 3 && $month -le 6 ]]; then
+	if [[ $month -eq 3 && $day -le 19 || $month -eq 6 && $day -ge 21 ]]; then
+		echo "False"
+	elif [[ $month%2 -eq 1 && $day -ge 1 && $day -le 31 || $month%2 -eq 0 && $day -ge 1 && $day -le 30 ]]; then
+		echo "True"
+	else
+		echo "False"
+	fi
+else
+	echo "False"
+fi
